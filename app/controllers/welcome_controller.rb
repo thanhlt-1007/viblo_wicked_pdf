8263a59da4c92c1d3edc801b4fe5ea575f77bcd4
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2021-08-22 20:55:35 +0700

    Options

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,13 @@
 class WelcomeController < ApplicationController
   def index
+    @mesage = "Hello world"
     respond_to do |format|
       format.html
       format.pdf do
-        render pdf: "file_name"   # Excluding ".pdf" extension.
+        render pdf: "welcome",
+               disposition: "inline",
+               show_as_html: params["debug"] == "true",
+               orientation: "Landscape"
       end
     end
   end
